29a315c05120bcb6b4e91feff1712bf086000578
xaviprat <xaviprat@live.unc.edu>
2021-12-02 20:11:21 -0500

    all at once!

--- server.js
+++ server.js
@@ -43,20 +43,22 @@ app.get("/app/user/:id", (req,res) => {
 })
 // UPDATE a single user (HTTP method PATCH) at endpoint /app/update/user/:id
 
-// app.get("/app/update/user/:id", req, res => {
-// 	const stmt = db.prepare("UPDATE userinfo SET user = COALESCE(?, user), pass = COALESCE(?,pass) WHERE id=?")
-// 	const info = stmt.run(req.body.user, md5(req.body.pass), req.params.id);
-// 	res.status(200).json({"message": `1 record updated: ID ${req.params.id} (200)`});
-// });
+app.get("/app/update/user/:id", req, res => {
+	const stmt = db.prepare("UPDATE userinfo SET user = COALESCE(?, user), pass = COALESCE(?,pass) WHERE id=?")
+	const info = stmt.run(req.body.user, md5(req.body.pass), req.params.id);
+	res.status(200).json({"message": `1 record updated: ID ${req.params.id} (200)`});
+});
 // DELETE a single user (HTTP method DELETE) at endpoint /app/delete/user/:id
-
-// Default response for any other request
 
 app.delete("/app/delete/user/:id", (req, res) => {
 	const stmt = db.prepare("DELETE FROM userinfo WHERE id = ?")
 	const info = stmt.run(req.params.id);
 	res.status(200).json({"message": info. changes + " record deleted: ID " + req.params.id + " (200)"})
 })
+
+// Default response for any other request
+
+
 app.use(function(req, res){
 	res.json({"message":"Your API is working!"});
     res.status(404);
