442838a079170523e868dd1d8101e575cafffa78
xaviprat <xaviprat@live.unc.edu>
2021-12-02 19:20:41 -0500

    create part

--- server.js
+++ server.js
@@ -23,7 +23,11 @@ app.get("/app/", (req, res, next) => {
 
 // Define other CRUD API endpoints using express.js and better-sqlite3
 // CREATE a new user (HTTP method POST) at endpoint /app/new/
-
+app.post("/app/new", (req, res) => {
+	const stmt = db.prepare("INSERT INTO userinfo (user, pass) VALUES (?, ?");
+	const info = stmt.run(req.body.user, md5(req.body.pass));
+	res.status(201).json({"message": `1 record created: ID ${info.lastInsertRowid} (201)`});
+});
 // READ a list of all users (HTTP method GET) at endpoint /app/users/
 app.get("/app/users", (req, res) => {	
 	const stmt = db.prepare("SELECT * FROM userinfo").all();
