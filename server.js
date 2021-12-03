32cf015987a985dd7d051249aed0a0d8d8f95b35
xaviprat <xaviprat@live.unc.edu>
2021-12-03 16:11:17 -0500

    crossed fingers

--- server.js
+++ server.js
@@ -13,7 +13,8 @@ app.use(express.json());
 // Set server port
 var HTTP_PORT = 5000
 // Start server
-app.listen(HTTP_PORT, () => {
+const server = app.listen(HTTP_PORT, () => {
+	
     console.log("Server running on port %PORT%".replace("%PORT%",HTTP_PORT))
 });
 // READ (HTTP method GET) at root endpoint /app/
