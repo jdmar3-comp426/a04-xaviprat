602ad76ea30af67996c68b515e8a0cfc9f8a6146
xaviprat <xaviprat@live.unc.edu>
2021-12-03 16:37:16 -0500

    this one hold up

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ app.get("/app/user/:id", (req,res) => {
 })
 // UPDATE` a single user (HTTP method PATCH) at endpoint /app/update/user/:id
 
-app.patch("/app/update/user/:id", req, res => {
+app.patch("/app/update/user/:id", (req, res) => {
 	const stmt = db.prepare("UPDATE userinfo SET user = COALESCE(?, user), pass = COALESCE(?,pass) WHERE id=?")
 	const info = stmt.run(req.body.user, md5(req.body.pass), req.params.id);
 	res.status(200).json({"message": info.changes + " record updated: ID " + req.params.id + " (200)"});
